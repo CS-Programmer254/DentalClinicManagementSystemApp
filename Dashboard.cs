082ab6dc7843ec16360fe8903511e921e8eed006
Stanley <stanleigh3oduor@gmail.com>
2024-01-13 23:01:27 +0300

    generated models using ADO.NET

--- Dashboard.cs
+++ Dashboard.cs
@@ -7,9 +7,7 @@ namespace DentalClinicManagementSystemApp
 {
     public partial class Dashboard : Form
     {
-        SqlConnection connection = new SqlConnection("Data Source=DEKTOP-KHILI9C-\\SQLEXPRESS;Initial Catalog=DentalClinicDb;Integrated Security=True");
-        private string display = "";
-        string query;
+        
         public Dashboard()
         {
             InitializeComponent();
@@ -17,30 +15,33 @@ namespace DentalClinicManagementSystemApp
         private void patientsBtn_Click(object sender, EventArgs e)
         {
             groupBox.Text = "Registered Patients";
-        
+           
+
         }
 
         private void doctorsBtn_Click(object sender, EventArgs e)
         {
             groupBox.Text = "Doctors List View";
+           
         }
 
         private void appointmentsBtn_Click(object sender, EventArgs e)
         {
-            groupBox.Text = "Appointment Bookings";         
+            groupBox.Text = "Appointment Bookings";
+            
 
         }
 
         private void insurancesBtn_Click(object sender, EventArgs e)
         {
             groupBox.Text = "List of Insurances";
-          
+           
         }
 
         private void billingBtn_Click(object sender, EventArgs e)
         {
             groupBox.Text = "Recent Billings";
-            
+           
         }
 
         private void Dashboard_Load(object sender, EventArgs e)
@@ -49,12 +50,7 @@ namespace DentalClinicManagementSystemApp
         }
         private void PopulateGridView()
         {
-            connection.Open();
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, connection);
-            DataTable dataTable = new DataTable();
-            sqlDataAdapter.Fill(dataTable);
-            DataGridView.DataSource = dataTable;
-            connection.Close();
+            
 
         }
 
