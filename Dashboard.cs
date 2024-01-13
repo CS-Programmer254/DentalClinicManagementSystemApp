cbfa43d55c9cbb00e897cc999f05e556eba5bbec
Stanley <stanleigh3oduor@gmail.com>
2024-01-13 18:31:45 +0300

    managed event handling

--- Dashboard.cs
+++ Dashboard.cs
@@ -17,33 +17,30 @@ namespace DentalClinicManagementSystemApp
         private void patientsBtn_Click(object sender, EventArgs e)
         {
             groupBox.Text = "Registered Patients";
-            display = "patientsBtn";
-
+        
         }
 
         private void doctorsBtn_Click(object sender, EventArgs e)
         {
             groupBox.Text = "Doctors List View";
-            display = "doctorsBtn";
         }
 
         private void appointmentsBtn_Click(object sender, EventArgs e)
         {
-            groupBox.Text = "Appointment Bookings";
-            display = "appointmentsBtn";
+            groupBox.Text = "Appointment Bookings";         
 
         }
 
         private void insurancesBtn_Click(object sender, EventArgs e)
         {
             groupBox.Text = "List of Insurances";
-            display = "insurancesBtn";
+          
         }
 
         private void billingBtn_Click(object sender, EventArgs e)
         {
             groupBox.Text = "Recent Billings";
-            display = "billingsBtn";
+            
         }
 
         private void Dashboard_Load(object sender, EventArgs e)
@@ -53,31 +50,6 @@ namespace DentalClinicManagementSystemApp
         private void PopulateGridView()
         {
             connection.Open();
-            if (display.Equals("patientsBtn")) {
-
-                query = "SELECT*FROM patient_Table";
-
-            }else if (display.Equals("doctorsBtn"))
-            {
-               // query = "SELECT*FROM patient_Table";
-
-            }else if (display.Equals("appointmentsBtn"))
-            {
-               // query = "";
-
-            }else if (display.Equals("insurancesBtn"))
-            {
-               // query = "";
-
-            }
-            else if (display.Equals("billingsBtn"))
-            {
-                //query = "";
-
-            }
-            else{
-                query = "SELECT*FROM patient_Table";
-            }
             SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, connection);
             DataTable dataTable = new DataTable();
             sqlDataAdapter.Fill(dataTable);
